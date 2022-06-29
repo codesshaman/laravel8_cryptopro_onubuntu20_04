fafa501d16ac4868655220ede057049bea329f64
Nick <90702473+codesshaman@users.noreply.github.com>
2022-06-29 18:49:03 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ read -p "Введите порт для laravel: " LARAVEL_PORT
 sed -i "s/80:80/${LARAVEL_PORT}:80/g" docker-compose.yml
 sed -i "s/1000:1000/${USER_ID}:${USER_ID}/g" docker-compose.yml
 composer create-project laravel/laravel laravel
+docker-compose up -d --build
 cd laravel
 composer update
 cd ../
-docker-compose up -d --build
